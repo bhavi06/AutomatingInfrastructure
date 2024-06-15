e6517ba55146b45d4d9749c446747148d4292901
bhavi06 <58685725+bhavi06@users.noreply.github.com>
2024-06-15 18:36:48 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,22 @@
 variable "region"{
     type = string
+    default = "us-east-1"
 }
 
-variable "local_ip"{
-    type = string
-}
 
 variable "ec2name" {
     type = string
+    default = "devserver"
 }
 
 variable "ami" {
     type = string
+    default = "ami-0069eac59d05ae12b"
 }
 
 variable "instance_type" {
     type = string
+default = "t2.micro"
 }
+
+
